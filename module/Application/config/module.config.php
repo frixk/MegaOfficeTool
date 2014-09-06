8b07459301a1e1a677468246cbb92236ee0caf1e
gabbydgab <gamba@gabbydgab.com>
2014-09-07 00:28:52 +0800

    updated MOT layout template

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -45,6 +45,7 @@ return [
         'exception_template'        => 'error/index',
         'template_map'              => [
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'template/footer'           => __DIR__ . '/../view/layout/partial/footer.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
